00748fd84519da9fee4d310d92dd5f5d0ccdc90a
DeMaT87 <92327438+DeMaT87@users.noreply.github.com>
2023-12-19 21:46:56 -0300

    Actualizacion Modulo 5

---
+++ Proyecto_Web/js/codigo.js
@@ -0,0 +1,15 @@
+function abrir() {
+  document.getelementById("desplegable").style.display = "flex";
+}
+
+function cerrar() {
+  document.getElementById("desplegable").style.display = "none";
+}
+
+function abrirModal() {
+    document.getElementById("modal").style.display = "flex";
+}
+
+function cerrarModal() {
+    document.getElementById("modal").style.display = "none";
+}
